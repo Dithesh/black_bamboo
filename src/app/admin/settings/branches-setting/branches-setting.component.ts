f43905882df0b0e0fd6c923c90c12f7febd02c90
godreamt <48436978+godreamt@users.noreply.github.com>
2020-06-28 13:46:51 +0530

    branches finished

--- src/app/admin/settings/branches-setting/branches-setting.component.ts
+++ src/app/admin/settings/branches-setting/branches-setting.component.ts
@@ -2,22 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DataService } from 'src/app/shared/services/data.service';
 
-export interface PeriodicElement {
-  action:any;
-  title: string;
-  discription: string;
-  address: string;
-  branchcode:  number;
-}
-
-const TABLE_DATA: PeriodicElement[] = [
-  {action:'', title: 'Tabel', discription: 'delivered', address: 'address here', branchcode: 122345},
-  {action:'', title: 'Parcel', discription: 'delivered', address: 'address here', branchcode: 122345},
-  {action:'', title: 'Taken', discription: 'delivered', address: 'address here', branchcode: 122345},
-  {action:'', title: 'table', discription: 'delivered', address: 'address here', branchcode: 122345},
-  {action:'', title: 'Hydrogen', discription: 'delivered', address: 'address here', branchcode: 122345},
-];
 
 @Component({
   selector: 'app-branches-setting',
@@ -26,17 +13,79 @@ const TABLE_DATA: PeriodicElement[] = [
 })
 export class BranchesSettingComponent implements OnInit {
 
-  displayedColumns: string[] = ['action', 'title', 'discription', 'address', 'branchcode'];
-  dataSource = new MatTableDataSource(TABLE_DATA);
+  displayedColumns: string[] = ['action', 'branchCode', 'branchTitle', 'description', 'branchAddress', 'isActive'];
+  dataSource;
   sidemenu=false;
-  
+  form:FormGroup;
+
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  constructor() { }
+  constructor(
+    private _serv: DataService,
+    private fb: FormBuilder
+  ) { 
+    this.form = this.fb.group({
+      id: [''],
+      branchCode: [''],
+      branchTitle: [''],
+      description: [''],
+      branchAddress: [''],
+      isActive: [false]
+    })
+  }
 
   ngOnInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+    // this.dataSource.paginator = this.paginator;
+    this.getAllBranches();
+  }
+
+  getAllBranches(page=1) {
+    this._serv.endpoint = "order-manager/branch?pageNumber="+page;
+    this._serv.get().subscribe(response => {
+      this.dataSource = response as any;
+      
+    })
+  }
+
+  changeStatus(data) {
+    this._serv.endpoint="order-manager/branch/status/"+data.id;
+    this._serv.put(data).subscribe(response => {
+          
+    })
+  }
+
+  updateBranch(item = null) {
+    if(item == null) {
+      this.form.reset();
+    }else {
+      this.form.patchValue(item);
+    }
+    this.sidemenu = true;
+  }
+
+  cancelUpdate() {
+    this.sidemenu=false;
+    this.form.reset();
+  }
+
+  saveBranch(event=null) {
+    if(event!=null)event.preventDefault();
+    this.form.markAllAsTouched();
+    if(this.form.invalid)return;
+    let formValue = this.form.value;
+    this._serv.endpoint="order-manager/branch";
+    let apiCall=null;
+    if(formValue.id && formValue.id != null && formValue.id !=undefined){
+      this._serv.endpoint+='/'+formValue.id;
+      apiCall = this._serv.put(formValue);
+    }else {
+      apiCall = this._serv.post(formValue);
+    }
+    apiCall.subscribe(response => {
+      this.cancelUpdate();
+      this.getAllBranches();
+    })
   }
 
 }
--- src/app/admin/settings/categories-setting/categories-setting.component.ts
+++ src/app/admin/settings/categories-setting/categories-setting.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { DataService } from 'src/app/shared/services/data.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-categories-setting',
@@ -10,21 +11,33 @@ import { DataService } from 'src/app/shared/services/data.service';
   styleUrls: ['./categories-setting.component.scss']
 })
 export class CategoriesSettingComponent implements OnInit {
-  displayedColumns: string[] = ['action', 'categoryName', 'description', 'isActive'];
+  displayedColumns: string[] = ['action', 'categoryName', 'description', 'branch', 'isActive'];
   dataSource;
   sidemenu=false;
+  form:FormGroup;
+  branchList: any[];
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor(
-    private _serv: DataService
-  ) { }
+    private _serv: DataService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      id: [''],
+      categoryName: [''],
+      description: [''],
+      branch_id: [''],
+      isActive: [false]
+    })
+   }
 
   ngOnInit(): void {
     // this.dataSource.sort = this.sort;
     // this.dataSource.paginator = this.paginator;
 
-    this.getAllCategories()
+    this.getAllCategories();
+    this.getAllBranches();
   }
 
   getAllCategories(page=1) {
@@ -38,7 +51,51 @@ export class CategoriesSettingComponent implements OnInit {
   changeStatus(data) {
     this._serv.endpoint="order-manager/category/status/"+data.id;
     this._serv.put(data).subscribe(response => {
-      
+          
+    })
+  }
+
+  updateCategory(item = null) {
+    if(item == null) {
+      this.form.reset();
+      if(this.branchList.length > 0) {
+        this.form.get('branch_id').setValue(this.branchList[0].id)
+      }
+    }else {
+      this.form.patchValue(item);
+
+    }
+    this.sidemenu = true;
+  }
+
+  cancelUpdate() {
+    this.sidemenu=false;
+    this.form.reset();
+  }
+
+  saveCategory(event=null) {
+    if(event!=null)event.preventDefault();
+    this.form.markAllAsTouched();
+    if(this.form.invalid)return;
+    let formValue = this.form.value;
+    this._serv.endpoint="order-manager/category";
+    let apiCall=null;
+    if(formValue.id && formValue.id != null && formValue.id !=undefined){
+      this._serv.endpoint+='/'+formValue.id;
+      apiCall = this._serv.put(formValue);
+    }else {
+      apiCall = this._serv.post(formValue);
+    }
+    apiCall.subscribe(response => {
+      this.cancelUpdate();
+      this.getAllCategories();
+    })
+  }
+
+  getAllBranches() {
+    this._serv.endpoint = "order-manager/branch?fields=id,branchTitle";
+    this._serv.get().subscribe(response => {
+      this.branchList = response as any[];
     })
   }
 
