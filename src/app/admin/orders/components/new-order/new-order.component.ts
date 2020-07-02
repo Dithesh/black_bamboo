90030036131686393d040c0dfd0db0e64e9457ab
Deekshith shetty <58114192+Dithesh@users.noreply.github.com>
2020-07-03 00:18:34 +0530

    update order new page

--- src/app/admin/orders/components/new-order/new-order.component.ts
+++ src/app/admin/orders/components/new-order/new-order.component.ts
@@ -3,23 +3,15 @@ import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  product: string;
+  quantity: number;
+  price: number;
+  total: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {product: 1, quantity: 'Hydrogen', price: 1.0079, total: 10},
+  {product: 2, quantity: 'Helium', price: 4.0026, total: 12},
 ];
 
 @Component({
@@ -43,7 +35,7 @@ export class NewOrderComponent implements OnInit {
   }
  
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['product', 'quantity', 'price', 'total'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
