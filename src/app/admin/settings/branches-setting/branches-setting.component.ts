0d3fbf5cc74a60ec5bada3db81e8b9f9e49e03ce
godreamt <48436978+godreamt@users.noreply.github.com>
2020-07-02 22:17:20 +0530

    Branch updated

--- src/app/admin/settings/branches-setting/branches-setting.component.ts
+++ src/app/admin/settings/branches-setting/branches-setting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
@@ -6,6 +6,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { DataService } from 'src/app/shared/services/data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
+import { merge } from 'rxjs';
 
 
 @Component({
@@ -13,20 +14,26 @@ import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/c
   templateUrl: './branches-setting.component.html',
   styleUrls: ['./branches-setting.component.scss']
 })
-export class BranchesSettingComponent implements OnInit {
+export class BranchesSettingComponent implements OnInit, AfterViewInit {
 
   displayedColumns: string[] = ['action', 'branchCode', 'branchTitle', 'description', 'branchAddress', 'isActive'];
   dataSource;
   sidemenu=false;
   form:FormGroup;
+  filterForm:FormGroup;
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private _serv: DataService,
     private fb: FormBuilder,
     private dialog: MatDialog
   ) { 
+    this.filterForm = this.fb.group({
+      searchString: [''],
+      orderCol: [''],
+      orderType: ['']
+    });
     this.form = this.fb.group({
       id: [''],
       branchCode: [''],
@@ -43,18 +50,31 @@ export class BranchesSettingComponent implements OnInit {
     this.getAllBranches();
   }
 
+  ngAfterViewInit() {
+    
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    merge(this.sort.sortChange, this.paginator.page, this.filterForm.get('searchString').valueChanges)
+      .subscribe(data => {
+        this.filterForm.patchValue({
+          orderCol: this.sort.active,
+          orderType: this.sort.direction
+        })
+        this.getAllBranches(this.paginator.pageIndex + 1)
+      });
+  }
+
   getAllBranches(page=1) {
-    this._serv.endpoint = "order-manager/branch?pageNumber="+page;
+    let filterValue=this.filterForm.value;
+    this._serv.endpoint = "order-manager/branch?pageNumber="+page+"&orderType="+filterValue.orderType+"&orderCol="+filterValue.orderCol+"&searchString="+filterValue.searchString;
     this._serv.get().subscribe(response => {
       this.dataSource = response as any;
-      
     })
   }
 
   changeStatus(data) {
     this._serv.endpoint="order-manager/branch/status/"+data.id;
     this._serv.put(data).subscribe(response => {
-          
+      this._serv.showMessage("Branch status changed successfully", 'success');
     })
   }
 
@@ -93,6 +113,15 @@ export class BranchesSettingComponent implements OnInit {
   }
 
   deleteBranch(item) {
-    this.dialog.open(ConfirmPopupComponent);
+    let dialogRef = this.dialog.open(ConfirmPopupComponent);
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this._serv.endpoint = "order-manager/branch/"+item.id;
+        this._serv.delete().subscribe(response => {
+          this._serv.showMessage("Branch deleted successfully", 'success');
+          this.getAllBranches();
+        })
+      }
+    })
   }
 }
--- src/app/admin/settings/categories-setting/categories-setting.component.ts
+++ src/app/admin/settings/categories-setting/categories-setting.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { DataService } from 'src/app/shared/services/data.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-categories-setting',
   templateUrl: './categories-setting.component.html',
   styleUrls: ['./categories-setting.component.scss']
 })
-export class CategoriesSettingComponent implements OnInit {
+export class CategoriesSettingComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['action', 'categoryName', 'description', 'branch', 'isActive'];
   dataSource;
   sidemenu=false;
   form:FormGroup;
+  filterForm:FormGroup;
   branchList: any[];
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private _serv: DataService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private dialog: MatDialog
   ) {
+    this.filterForm = this.fb.group({
+      searchString: [''],
+      orderCol: [''],
+      orderType: ['']
+    });
     this.form = this.fb.group({
       id: [''],
       categoryName: [''],
@@ -33,25 +43,35 @@ export class CategoriesSettingComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-
     this.getAllCategories();
     this.getAllBranches();
   }
 
+  ngAfterViewInit() {
+    
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    merge(this.sort.sortChange, this.paginator.page, this.filterForm.get('searchString').valueChanges)
+      .subscribe(data => {
+        this.filterForm.patchValue({
+          orderCol: this.sort.active,
+          orderType: this.sort.direction
+        })
+        this.getAllCategories(this.paginator.pageIndex + 1)
+      });
+  }
+
   getAllCategories(page=1) {
-    this._serv.endpoint = "order-manager/category?pageNumber="+page;
+    let filterValue=this.filterForm.value;
+    this._serv.endpoint = "order-manager/category?pageNumber="+page+"&orderType="+filterValue.orderType+"&orderCol="+filterValue.orderCol+"&searchString="+filterValue.searchString;
     this._serv.get().subscribe(response => {
       this.dataSource = response as any;
-      
     })
   }
 
   changeStatus(data) {
     this._serv.endpoint="order-manager/category/status/"+data.id;
     this._serv.put(data).subscribe(response => {
-          
+      this._serv.showMessage("Category status changed successfully", 'success');
     })
   }
 
@@ -87,6 +107,7 @@ export class CategoriesSettingComponent implements OnInit {
       apiCall = this._serv.post(formValue);
     }
     apiCall.subscribe(response => {
+      this._serv.showMessage("Category updated successfully", 'success');
       this.cancelUpdate();
       this.getAllCategories();
     })
@@ -96,6 +117,19 @@ export class CategoriesSettingComponent implements OnInit {
     this._serv.endpoint = "order-manager/branch?fields=id,branchTitle";
     this._serv.get().subscribe(response => {
       this.branchList = response as any[];
+    })
+  }
+
+  deleteCategory(item) {
+    let dialogRef = this.dialog.open(ConfirmPopupComponent);
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this._serv.endpoint = "order-manager/category/"+item.id;
+        this._serv.delete().subscribe(response => {
+          this._serv.showMessage("Category deleted successfully", 'success');
+          this.getAllCategories();
+        })
+      }
     })
   }
 
