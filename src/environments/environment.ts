72f1bded263ac37c74837c50493c9f9f2b2a905e
godreamt <48436978+godreamt@users.noreply.github.com>
2020-07-04 09:51:09 +0530

    mat slide toggle cutomised

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "http://127.0.0.1:8000/api/",
-  apiUrl: "http://godreamt.com/resto-app/public/api/"
+  apiUrl: "http://127.0.0.1:8000/api/",
+  // apiUrl: "http://godreamt.com/resto-app/public/api/"
 };
 
 /*
