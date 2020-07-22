e99e091cedaa09f741a55cfadfbb1b5deca9e227
godreamt <48436978+godreamt@users.noreply.github.com>
2020-07-22 21:29:43 +0530

    Master pages finished

--- src/app/admin/account/master/inventory/inventory.component.ts
+++ src/app/admin/account/master/inventory/inventory.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { DataService } from 'src/app/shared/services/data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
 
 @Component({
   selector: 'app-inventory',
@@ -6,10 +10,117 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  editIndex: any;
+  unitList: any[] =[];
+  constructor(
+    private fb: FormBuilder,
+    private _serv: DataService,
+    private dialog: MatDialog
+  ) { 
+    this.form = this.fb.group({
+      inventories: this.fb.array([]),
+      newInventories: this.fb.array([]),
+    })
+  }
 
   ngOnInit(): void {
+    this.getAllUnits();
+    this.getAllInventories();
+  }
+
+  getAllUnits() {
+    this._serv.endpoint="account-manager/unit";
+    this._serv.get().subscribe((response:any[]) => {
+      this.unitList = response;
+    })
+  }
+
+  get inventories() {
+    return this.form.get('inventories') as FormArray;
+  }
+
+  get newInventories() {
+    return this.form.get('newInventories') as FormArray;
+  }
+
+  getAllInventories() {
+    this.inventories.controls = [];
+    this.inventories.reset();
+    this._serv.endpoint = "account-manager/inventory";
+    this._serv.get().subscribe((response: any[]) => {
+      response.forEach(elem => {
+        let form = this.newInventory();
+        form.patchValue(elem);
+        this.inventories.push(form)
+      })
+      if(response.length == 0){
+        this.newInventories.push(this.newInventory());
+      }
+    })
+  }
+
+  addNewInventory() {
+    this.newInventories.push(this.newInventory());
+  }
+
+  removeNewInventory(index) {
+    this.newInventories.removeAt(index);
+  }
+
+  newInventory() {
+    return this.fb.group({
+      id: [''],
+      itemName: ['', [Validators.required]],
+      unitId: ['', [Validators.required]],
+      description: [''],
+      pricePerUnit: ['0'],
+      isActive: [true],
+    });
+  }
+
+  saveInventory(inventoryForm: FormGroup, index=null) {
+    inventoryForm.markAllAsTouched();
+    if(inventoryForm.invalid)return;
+    let data = {...inventoryForm.value};
+
+    
+    let api=null;
+    this._serv.endpoint="account-manager/inventory";
+    if(data.id == "") {
+      api = this._serv.post(data);
+    }else {
+      this._serv.endpoint+="/"+data.id;
+      api = this._serv.put(data);
+    }
+    api.subscribe(response => {
+      if(data.id == "" && index != null) {
+        this.newInventories.removeAt(index);
+        this.getAllInventories();
+      }else {
+        this.editIndex=undefined;
+      }
+    })
+  }
+
+  onEditClick(index) {
+    this.editIndex= index;
+  }
+
+  deleteInventory(index) {
+    let item = this.inventories.controls[index].value;
+    let dialogRef = this.dialog.open(ConfirmPopupComponent);
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this._serv.endpoint = "account-manager/inventory/"+item.id;
+        this._serv.delete().subscribe(response => {
+          this._serv.showMessage("Inventory deleted successfully", 'success');
+          this.inventories.removeAt(index);
+        }, ({error}) => {
+          this._serv.showMessage(error['msg'], 'error');
+        })
+      }
+    })
   }
 
 }
--- src/app/admin/account/master/units/units.component.ts
+++ src/app/admin/account/master/units/units.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { DataService } from 'src/app/shared/services/data.service';
+import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-units',
@@ -6,10 +10,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./units.component.scss']
 })
 export class UnitsComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  editIndex: any;
+  constructor(
+    private fb: FormBuilder,
+    private _serv: DataService,
+    private dialog: MatDialog
+  ) { 
+    this.form = this.fb.group({
+      units: this.fb.array([]),
+      newUnits: this.fb.array([]),
+    })
+  }
 
   ngOnInit(): void {
+    this.getAllUnits();
+  }
+
+  get units() {
+    return this.form.get('units') as FormArray;
+  }
+
+  get newUnits() {
+    return this.form.get('newUnits') as FormArray;
+  }
+
+  getAllUnits() {
+    this.units.controls = [];
+    this.units.reset();
+    this._serv.endpoint = "account-manager/unit";
+    this._serv.get().subscribe((response: any[]) => {
+      response.forEach(elem => {
+        let form = this.newUnit();
+        form.patchValue(elem);
+        this.units.push(form)
+      })
+      if(response.length == 0){
+        this.newUnits.push(this.newUnit());
+      }
+    })
+  }
+
+  addNewUnit() {
+    this.newUnits.push(this.newUnit());
+  }
+
+  removeNewUnit(index) {
+    this.newUnits.removeAt(index);
+  }
+
+  newUnit() {
+    return this.fb.group({
+      id: [''],
+      unitLabel: ['', [Validators.required]],
+      description: ['']
+    });
+  }
+
+  saveUnit(unitForm: FormGroup, index=null) {
+    unitForm.markAllAsTouched();
+    if(unitForm.invalid)return;
+    let data = {...unitForm.value};
+
+    
+    let api=null;
+    this._serv.endpoint="account-manager/unit";
+    if(data.id == "") {
+      api = this._serv.post(data);
+    }else {
+      this._serv.endpoint+="/"+data.id;
+      api = this._serv.put(data);
+    }
+    api.subscribe(response => {
+      if(data.id == "" && index != null) {
+        this.newUnits.removeAt(index);
+        this.getAllUnits();
+      }else {
+        this.editIndex=undefined;
+      }
+    })
+  }
+
+  onEditClick(index) {
+    this.editIndex= index;
+  }
+
+  deletUnit(index) {
+    let item = this.units.controls[index].value;
+    let dialogRef = this.dialog.open(ConfirmPopupComponent);
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this._serv.endpoint = "account-manager/unit/"+item.id;
+        this._serv.delete().subscribe(response => {
+          this._serv.showMessage("Unit deleted successfully", 'success');
+          this.units.removeAt(index);
+        }, ({error}) => {
+          this._serv.showMessage(error['msg'], 'error');
+        })
+      }
+    })
   }
 
 }
