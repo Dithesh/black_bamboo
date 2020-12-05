054d0b60d8c9751695da1e561154a4f07153daa5
Dithesh <58114192+Dithesh@users.noreply.github.com>
2020-12-05 16:41:31 +0530

    update report design

--- src/app/admin/account-management/account-reports/custom-report/custom-report.component.ts
+++ src/app/admin/account-management/account-reports/custom-report/custom-report.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DataService } from './../../../../shared/services/data.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-custom-report',
@@ -7,9 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomReportComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  displayedColumns: string[] = ['action', 'branchCode', 'branchTitle', 'description', 'branchAddress', 'isActive'];
+  dataSource;
+  filterForm:FormGroup;
+  companyList;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(
+    private _serv: DataService,
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    private route:ActivatedRoute
+  ) { 
+    this.filterForm = this.fb.group({
+      type: [''],
+      includeOrder:[false],
+      startDate:[''],
+      endDate:[''],
+      orderCol: [''],
+      orderType: [''],
+      companyFilter:['']
+    });
+    // this.route.data.subscribe(response => {
+    //   this.companyList = response.companyList;
+    //   if(this.companyList.length > 0){
+    //     this.filterForm.get('companyFilter').setValue(this.companyList[0].id);
+    //   }
+    // })
   }
 
+  ngOnInit(): void {
+    // this.dataSource.sort = this.sort;
+    // this.dataSource.paginator = this.paginator;
+    // this.getAllBranches();
+  }
+  
+  ngAfterViewInit() {
+    
+    // this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    // merge(this.sort.sortChange, this.paginator.page, this.filterForm.get('searchString').valueChanges)
+    //   .subscribe(data => {
+    //     this.filterForm.patchValue({
+    //       orderCol: this.sort.active,
+    //       orderType: this.sort.direction
+    //     })
+    //     this.getAllBranches(this.paginator.pageIndex + 1)
+    //   });
+  }
+
+  // getAllBranches(page=1) {
+  //   let filterValue=this.filterForm.value;
+  //   this._serv.endpoint = "order-manager/branch?pageNumber="+page+"&orderType="+filterValue.orderType+"&orderCol="+filterValue.orderCol+"&searchString="+filterValue.searchString+'&compnayId='+filterValue.companyFilter;
+  //   this._serv.get().subscribe(response => {
+  //     this.dataSource = response as any;
+  //   })
+  // }
 }
